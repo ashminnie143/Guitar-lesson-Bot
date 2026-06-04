3dc6bceeeb9219a020dcce323f6849307f80d182
ashminnie143 <ashsan1620@gmail.com>
2026-06-04 16:50:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post("/webhook", async (req, res) => {
 
 async function sendMessage(to, text) {
   await axios.post(
-    `https://graph.facebook.com/v19.0/${PHONE_NUMBER_ID}/messages`,
+    `https://graph.facebook.com/v25.0/${PHONE_NUMBER_ID}/messages`,
     {
       messaging_product: "whatsapp",
       to: to,
